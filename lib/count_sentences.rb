24493a1731f7ce3c9360acd0cb43273c1057f364
Mark <mark.nyambane@student.moringaschool.com>
2023-05-20 21:57:48 +0300

    Complete assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,30 @@
-require 'pry'
-
 class String
 
   def sentence?
-    
+
+     self[-1].end_with?'.'
+
+
   end
 
   def question?
 
+    self.end_with?'?'
   end
 
   def exclamation?
 
+    self.end_with?'!'
   end
 
   def count_sentences
 
+    self.split(/[!.?]\s[^!!..??]/).count
   end
 end
+end
+
+str = String.new('ase.')
+
+# puts str.sentence?
+puts 'This, well, is a sentence. This is too!! And so is this, I think? Woo...'.count_sentences
